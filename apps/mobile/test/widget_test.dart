2f78d2473d5b01bc14b8c3985b472bf881dfb6d7
pls455 <karamhahhss123@gmail.com>
2026-09-16 21:26:40 +0300

    fix Dakkana widget test import

--- apps/mobile/test/widget_test.dart
+++ apps/mobile/test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:dakkana_mobile/main.dart';
+import 'package:dakkana_mobile/app/app.dart';
 
 void main() {
   testWidgets('Dakkana app starts', (tester) async {
     await tester.pumpWidget(const DakkanaApp());
+    await tester.pump();
     expect(find.textContaining('دَكانة'), findsOneWidget);
-    expect(find.textContaining('كل حساب دَكانتك بإيدك'), findsOneWidget);
   });
 }
